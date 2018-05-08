72b076886d996f4a5236bed1f69a1746fdfbb02f
Jordan Hudgens <jordan@devcamp.com>
2018-05-07 19:56:51 -0500

    Generated the sqlite database

--- app.py
+++ app.py
@@ -5,9 +5,28 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hey Flask"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Guide(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=False)
+    content = db.Column(db.String(144), unique=False)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class GuideSchema(ma.Schema):
+    class Meta:
+        fields = ('title', 'content')
+
+
+guide_schema = GuideSchema()
+guides_schema = GuideSchema(many=True)
 
 
 if __name__ == '__main__':
