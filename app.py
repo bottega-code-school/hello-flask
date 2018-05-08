b2147866712998c8fbf7a0ae22035a82f5c73f38
Jordan Hudgens <jordan@devcamp.com>
2018-05-07 20:17:36 -0500

    Added the POST api endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -27,6 +27,21 @@ class GuideSchema(ma.Schema):
 
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
+
+# Endpoint to create a new guide
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
 
 
 if __name__ == '__main__':
