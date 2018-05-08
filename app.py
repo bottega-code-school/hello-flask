49437b139cf958b5c47e60f1dabacdc7b4616021
Jordan Hudgens <jordan@devcamp.com>
2018-05-07 20:57:14 -0500

    Built out update action endpoint

--- app.py
+++ app.py
@@ -59,6 +59,21 @@ def get_guide(id):
     return guide_schema.jsonify(guide)
 
 
+# Endpoint for updating a guide
+@app.route("/guide/<id>", methods=["PUT"])
+def guide_update(id):
+    guide = Guide.query.get(id)
+    title = request.json['title']
+    content = request.json['content']
+
+    guide.title = title
+    guide.content = content
+
+    db.session.commit()
+    return guide_schema.jsonify(guide)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
